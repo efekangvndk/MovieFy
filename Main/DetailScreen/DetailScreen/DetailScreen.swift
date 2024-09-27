f1a30007b7d3e56a857a2de1a7c69d1a5d464149
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-09-27 16:00:38 +0300

    fix

--- Main/DetailScreen/DetailScreen/DetailScreen.swift
+++ Main/DetailScreen/DetailScreen/DetailScreen.swift
@@ -9,6 +9,6 @@ import SwiftUI
 
 struct DetailScreen: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        Text("Hello")
     }
 }
